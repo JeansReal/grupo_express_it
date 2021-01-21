c62fd960873e52739453440aeec4e313a8bf8f9c
Jeans K. Real <jkrb7@hotmail.com>
2021-01-21 17:01:16 -0600

    Desk page

--- grupo_express_it/grupo_express_invoice_tool/doctype/sales_invoice/sales_invoice.js
+++ grupo_express_it/grupo_express_invoice_tool/doctype/sales_invoice/sales_invoice.js
@@ -1,31 +1,28 @@
 // Copyright (c) 2021, Agile Shift and contributors
 // For license information, please see license.txt
 
-function fetch_pricing_rule(frm, item_row) {
-
-
-
-}
-
-function calculate_item_amount(frm, cdt, cdn) {
-    let item = frappe.get_doc(cdt, cdn); // Getting The child Row being edited.
-
-    if (item.margin_type === 'Percentage') {
-        item.amount = (item.invoiced_amount / 100) * item.margin_rate_or_amount
-    } // TODO: Margin Type by Amount?
-
-    refresh_field('amount', cdn, 'items'); // Show change on 'amount' field. Without triggering any event.
-}
-
 function money_in_words(frm) {
     frappe.call({
         method: 'grupo_express_it.grupo_express_invoice_tool.doctype.sales_invoice.sales_invoice.money_in_words',
         args: {number: frm.doc.items[0].amount},
-        callback: (r) => {
-            frm.doc.in_words = r.message
-            frm.refresh_field('in_words');
-        }
+        callback: (r) => frm.doc.in_words = r.message,
+        async: false
     });
+}
+
+function calculate_item_amount(frm, cdt, cdn, item_row = null) {
+    if (!item_row) item_row = frappe.get_doc(cdt, cdn); // Getting item row being edited
+    if (!item_row.item) return;                         // Exit if there is no item
+
+    if (item_row.margin_type === 'Percentage') {
+        item_row.amount = (item_row.invoiced_amount / 100) * item_row.margin_rate_or_amount;
+    } else if (item_row.margin_type === 'Amount') {
+        item_row.amount = (item_row.qty * item_row.margin_rate_or_amount)
+    }
+
+    money_in_words(frm);
+
+    frm.refresh_fields(); // This finally refresh all the fields!
 }
 
 // Parent Doctype
@@ -48,34 +45,32 @@ frappe.ui.form.on('Sales Invoice', {
 // Child Doctype
 frappe.ui.form.on("Sales Invoice Item", {
     item: function (frm, cdt, cdn) {
-        let item_row = frappe.get_doc(cdt, cdn);
+        let item_row = frappe.get_doc(cdt, cdn); // Getting item row being edited
+
+        if (!item_row.item) return; // Exit if there is no item
 
         frappe.db.get_value(
             'Pricing Rule',
-            {
-                'parent': frm.doc.customer,
-                'item': item_row.item
-            },
+            {'parent': frm.doc.customer, 'item': item_row.item},
             ['margin_type', 'margin_rate_or_amount'],
             (price_rule) => {
                 item_row.margin_type = price_rule.margin_type;
                 item_row.margin_rate_or_amount = price_rule.margin_rate_or_amount;
+                calculate_item_amount(frm, cdt, cdn, item_row); // This calculates the item amount and the total.
             },
             'Customer'
         );
-
     },
 
-    // invoiced_amount: function (frm, cdt, cdn) {
-    //     calculate_item_amount(frm, cdt, cdn);
-    // },
+    invoiced_amount: function (frm, cdt, cdn) {
+        calculate_item_amount(frm, cdt, cdn);
+    },
 
-    // qty: function (frm, cdt, cdn) {
-    //     calculate_item_amount(frm, cdt, cdn);
-    // },
+    qty: function (frm, cdt, cdn) {
+        calculate_item_amount(frm, cdt, cdn);
+    },
 
     // margin_type: function (frm) { // TODO: Make this Work?
     //     frm.set_df_property('margin_rate_or_amount', 'description', frm.doc.margin_type == 'Percentage' ? 'In Percentage %' : 'In Amount');
     // }
 });
-//53 - 101 - 116 - 123 - 112 - 121 - 79
