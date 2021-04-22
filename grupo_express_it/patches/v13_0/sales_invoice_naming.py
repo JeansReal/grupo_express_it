66cdd4e18bd2bec195ee436a9639fc48d8b64fdd
Jeans K. Real <jkrb7@hotmail.com>
2021-04-21 22:02:24 -0600

    Creating a migration and more trans

---
+++ grupo_express_it/patches/v13_0/sales_invoice_naming.py
@@ -0,0 +1,16 @@
+import frappe
+
+
+def execute():
+    frappe.reload_doctype('Document Naming Rule')
+
+    doc = frappe.get_doc({
+        'doctype': 'Document Naming Rule',
+        'document_type': 'Sales Invoice',
+        'priority': 1,
+        'prefix': 'Recibo No. ',
+        'prefix_digits': 6,
+        'counter': 0
+    })
+
+    doc.insert()
