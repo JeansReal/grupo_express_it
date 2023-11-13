ba6564abe7e207ddae9c48c3d0f80c58b4878f78
Jeans K. Real <jkrb7@hotmail.com>
2023-11-13 13:44:51 -0600

    Print Format

--- grupo_express_it/stock_management/doctype/policy_item/policy_item.py
+++ grupo_express_it/stock_management/doctype/policy_item/policy_item.py
@@ -25,6 +25,6 @@ class PolicyItem(Document):
 		qty: DF.Float
 		total_price: DF.Currency
 		unit_price: DF.Currency
-		uom: DF.Literal["Unidad", "Docenas", "Millar", "Gruesas", "Yardas", "Metros", "Rollo", "Set"]
+		uom: DF.Literal["Unidad", "Docenas", "Millar", "Gruesas", "Yardas", "Metros", "Rollo", "Set", "Paquetes"]
 	# end: auto-generated types
 	pass
