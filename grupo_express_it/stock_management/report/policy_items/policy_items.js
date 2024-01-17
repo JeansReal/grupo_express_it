7a70facb72cf3bc4de4cf3da4fe8ba1b44b2192d
Jeans K. Real <jkrb7@hotmail.com>
2024-01-17 10:07:45 -0600

    Reports for Policy

--- grupo_express_it/stock_management/report/policy_items/policy_items.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, Agile Shift and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["Policy Items"] = {
-	"filters": [
-
-	]
-};
--- grupo_express_it/stock_management/report/policy_items/policy_items.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Agile Shift and contributors
-# For license information, please see license.txt
-
-# import frappe
-
-
-def execute(filters=None):
-	columns, data = [], []
-	return columns, data
